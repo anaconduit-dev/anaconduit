454271f21b79ed87e0a32d4127c5f1e7fddff0d6
anaconduit-dev <anaconduit@proton.me>
2026-01-24 21:44:49 +0200

    Update xray_api.py

--- app/core/xray_api.py
+++ app/core/xray_api.py
@@ -7,14 +7,14 @@ class XrayAPIClient:
     def __init__(self, address="host.docker.internal:10085"):
         self.address = address
 
-async def test_connection(self):
-    try:
-        async with grpc.aio.insecure_channel(self.address) as channel:
-            stub = stats_service.StatsServiceStub(channel)
-            # Вызов метода получения системной статистики
-            request = stats_types.GetSysStatsRequest()
-            response = await stub.GetSysStats(request)
-            return True if response.uptime > 0 else False
-    except Exception as e:
-        print(f"gRPC Error: {e}")
-        return False
+    async def test_connection(self):
+        try:
+            async with grpc.aio.insecure_channel(self.address) as channel:
+                stub = stats_service.StatsServiceStub(channel)
+                # Вызов метода получения системной статистики
+                request = stats_types.GetSysStatsRequest()
+                response = await stub.GetSysStats(request)
+                return True if response.uptime > 0 else False
+        except Exception as e:
+            print(f"gRPC Error: {e}")
+            return False
