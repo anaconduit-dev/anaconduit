78774a97e5ad1e99f52f92bca22adb8422f96a38
anaconduit-dev <anaconduit@proton.me>
2026-01-28 18:26:01 +0200

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,19 @@
 from pathlib import Path
-from pydantic_settings import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Anaconduit"
     debug: bool = False
     log_level: str = "INFO"
-    host_data_path: Path = Path("./data")
+    # Это значение подтянется из .env (HOST_DATA_PATH=/opt/anaconduit/data)
+    # Мы используем его для команд Docker SDK
+    host_data_path: str
 
-    class Config:
-        env_file = ".env"
-
+    # А это путь ВНУТРИ контейнера backend. 
+    # Так как в docker-compose ты прописал: /opt/anaconduit/data:/app/data
+    # то Python должен писать именно в /app/data
+    internal_data_path: Path = Path("/app/data")
+    
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore"б env_prefix="")
 
 settings = Settings()
