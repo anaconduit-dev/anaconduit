eafd834992aa88a5a68a2fcb7a0f0616a50e8b86
anaconduit-dev <anaconduit@proton.me>
2026-03-09 18:54:22 +0200

    Update xray_service.py

--- app/services/xray_service.py
+++ app/services/xray_service.py
@@ -124,7 +124,11 @@ class XrayService:
 
         # Базовая структура (переносим из твоего старого метода)
         config = {
-            "log": {"loglevel": "warning", "access": "none", "error": ""},
+            "log": {
+                    "loglevel": "debug",
+                    "access": "/var/log/xray/access.log",
+                    "error": "/var/log/xray/error.log"
+                },
             "stats": {},
             "api": {
                 "tag": "api",
