01c4ed0f9375101e06ac33587b2cf1e5048fdc55
anaconduit-dev <anaconduit@proton.me>
2026-03-12 21:28:24 +0200

    Add files via upload

--- app/api/endpoints/app.py
+++ app/api/endpoints/app.py
@@ -6,51 +6,17 @@ from app.core.database import get_db
 from app.core.config import settings
 import logging
 import secrets
-from app.core.dependencies import get_current_admin, get_update_service
+from app.core.dependencies import get_current_admin
 from app.schemas.system import UpdateRequest
 from typing import List, Optional
 from datetime import datetime, timedelta
 from app.core.logging import setup_logging
 from pydantic import BaseModel
-from app.services.update_service import UpdateService
 
-
-
-setup_logging(settings.log_level)
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-
-@router.post("/update")
-async def start_update(
-    data: UpdateRequest,
-    dmin: dict = Depends(get_current_admin),
-    update_service: UpdateService = Depends(get_update_service)
-    ):
-    """
-    Эндпоинт, который вызывает фронтенд при нажатии кнопки 'Обновить'
-    """
-    # 1. Проверяем, что гит смог переключить версию
-    # Мы используем await, потому что это быстрая операция
-    success = await update_service.apply_update(data.version_tag)
-    
-    if not success:
-        raise HTTPException(
-            status_code=500, 
-            detail="Не удалось переключить версию кода через Git"
-        )
-
-    # 2. Запускаем перезапуск контейнеров через 2 секунды,
-    # чтобы успеть отправить этот ответ фронтенду
-    import asyncio
-    loop = asyncio.get_event_loop()
-    loop.call_later(2, update_service.trigger_rebuild)
-
-    return {
-        "status": "success", 
-        "message": f"Код обновлен до {data.version_tag}. Система перезагружается..."
-    }
 
 
 @router.get("/status")
--- app/api/endpoints/clients.py
+++ app/api/endpoints/clients.py
@@ -15,10 +15,6 @@ import uuid
 from app.core.config import settings
 from app.core.logging import setup_logging
 
-
-
-
-setup_logging(settings.log_level)
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
