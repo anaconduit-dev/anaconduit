04ff29cda5de5f176938bf97d808e8db73b46ad3
anaconduit <anaconduit@proton.me>
2026-03-16 21:45:05 +0200

    fix: очередная альфа

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from pydantic import Field
 
 class Settings(BaseSettings):
     # --- Общие настройки ---
-    VERSION: str = "1.0.04"
+    VERSION: str = "1.0.05"
     GITHUB_REPO: str = "anaconduit-dev/anaconduit"
 
     app_name: str = "Anaconduit"
